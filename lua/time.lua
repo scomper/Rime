7e6bc57195ac674221cc7288f740890f8ec1e773
scomper <scomper@gmail.com>
2023-02-02 13:50:09 +0800

    颜色方案中增加新的透明参数

--- lua/time.lua
+++ lua/time.lua
@@ -4,7 +4,7 @@ time_translator: 将 `time` 翻译为当前时间
 
 local function translator(input, seg)
    if (input == "time") then
-      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
+      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), ""))
    end
 end
 
