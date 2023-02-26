b892d634d4e345dcb2d0e1ca7def3c0a60bfda82
scomper <scomper@gmail.com>
2023-02-26 08:26:52 +0800

    相关词库更新

--- lua/date.lua
+++ lua/date.lua
@@ -28,10 +28,13 @@ local function translator(input, seg)
             - comment: 候选项的注释
        --]]
       yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
+      yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
+      yield(Candidate("date", seg.start, seg._end, os.date("%Y.%m.%d"), ""))
+
       --[[ 用 `yield` 再产生一个候选项
            最终的效果是输入法候选框中出现两个格式不同的当前日期的候选项。
       --]]
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
+      yield(Candidate("date", seg.start, seg._end, os.date("%Y 年 %m 月 %d 日"), ""))
    end
 end
 
